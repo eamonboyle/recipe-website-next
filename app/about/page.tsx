26cd5d26965aae6ab2aee7085b0cb5d417cbfb5e
Eamon Boyle <blaowskate@hotmail.com>
2023-09-24 11:29:21 +0100

    Fix apostrophe error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,25 +6,25 @@ export default function Page() {
                     About Recipe Share
                 </h3>
                 <p className="text-lg mb-6">
-                    Welcome to Recipe Share, your go-to destination for all things delicious and culinary. We're passionate about bringing people together through the love of food. Our platform is a hub for food enthusiasts, both seasoned cooks and kitchen newcomers, to explore, create, and share the flavors of the world.
+                    Welcome to Recipe Share, your go-to destination for all things delicious and culinary. We&apos;re passionate about bringing people together through the love of food. Our platform is a hub for food enthusiasts, both seasoned cooks and kitchen newcomers, to explore, create, and share the flavors of the world.
                 </p>
                 <h3 className="text-2xl font-bold mb-3">
-                    Discover & Create
+                    Discover &amp; Create
                 </h3>
                 <p className="text-lg mb-6">
-                    Recipe Share is your culinary playground, where inspiration knows no bounds. Discover a treasure trove of mouthwatering recipes from every corner of the globe. Whether you're craving comfort food classics or seeking adventurous new tastes, we've got you covered. Our intuitive recipe creation tools make it a breeze to craft your own culinary masterpieces, so you can share your unique dishes with the world.
+                    Recipe Share is your culinary playground, where inspiration knows no bounds. Discover a treasure trove of mouthwatering recipes from every corner of the globe. Whether you&apos;re craving comfort food classics or seeking adventurous new tastes, we&apos;ve got you covered. Our intuitive recipe creation tools make it a breeze to craft your own culinary masterpieces, so you can share your unique dishes with the world.
                 </p>
                 <h3 className="text-2xl font-bold mb-3">
-                    Connect & Share
+                    Connect &amp; Share
                 </h3>
                 <p className="text-lg mb-6">
-                    Cooking is better when it's shared with friends. With Recipe Share, you'll connect with a vibrant community of fellow food lovers. Create your own profile, customize it to reflect your culinary style, and start following your favorite recipe creators. Stay up-to-date with their latest creations and never miss a beat in the culinary world.
+                    Cooking is better when it&apos;s shared with friends. With Recipe Share, you&apos;ll connect with a vibrant community of fellow food lovers. Create your own profile, customize it to reflect your culinary style, and start following your favorite recipe creators. Stay up-to-date with their latest creations and never miss a beat in the culinary world.
                 </p>
                 <h3 className="text-2xl font-bold mb-3">
-                    Celebrate Food & Community
+                    Celebrate Food &amp; Community
                 </h3>
                 <p className="text-lg mb-6">
-                    At Recipe Share, we believe that food has the power to connect people, spark creativity, and spread joy. Whether you're a home cook, a professional chef, or simply someone who appreciates a good meal, you're invited to be a part of our food-loving family. Together, we celebrate the joy of cooking, the beauty of diversity in cuisine, and the warmth of sharing a meal with loved ones.
+                    At Recipe Share, we believe that food has the power to connect people, spark creativity, and spread joy. Whether you&apos;re a home cook, a professional chef, or simply someone who appreciates a good meal, you&apos;re invited to be a part of our food-loving family. Together, we celebrate the joy of cooking, the beauty of diversity in cuisine, and the warmth of sharing a meal with loved ones.
 
                     Join us on this flavorful journey, where every recipe is a story waiting to be told, and every meal shared is a moment to remember. Explore, create, connect, and savor the magic of Recipe Share.
                 </p>
