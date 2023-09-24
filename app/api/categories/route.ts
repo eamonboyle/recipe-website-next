60a5e6ae3df6494851ec220eaeae0d1dd0dbbdb4
Eamon Boyle <blaowskate@hotmail.com>
2023-09-24 19:50:42 +0100

    Try out Azure sql route

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,7 +1,39 @@
 import { getAllCategories } from "@/app/controllers/category.controller";
-import { getDatabaseClient } from "@/services/database.service";
+import { connectToDatabase, getDatabaseClient } from "@/services/database.service";
 
 export async function GET(request: Request) {
+
+    try {
+        const pool = await connectToDatabase();
+        const result = await pool.query(`SELECT
+            c.id,
+            c.category AS name,
+            c.slug,
+            r.image_url
+        FROM
+            recipe_categories c
+        INNER JOIN (
+            SELECT
+                rtc.category_id,
+                MIN(CONVERT(VARCHAR(MAX), r.image_url)) AS image_url
+            FROM
+                recipes_to_categories rtc
+            JOIN
+                recipes r ON rtc.recipe_id = r.id
+            GROUP BY
+                rtc.category_id
+        ) AS r ON c.id = r.category_id
+        ORDER BY
+            c.category;`);
+        return new Response(JSON.stringify(result.recordset), { status: 200 })
+    }
+    catch (error) {
+        return new Response(JSON.stringify(error), { status: 500 })
+    }
+
+
+    return;
+
     const client = await getDatabaseClient();
 
     try {
--- services/database.service.ts
+++ services/database.service.ts
@@ -1,4 +1,5 @@
 import { Pool } from 'pg';
+import sql, { ConnectionPool } from 'mssql'
 
 const pool = new Pool({
     user: process.env.PUBLIC_POSTGRES_USER,
@@ -11,4 +12,24 @@ const pool = new Pool({
 export const getDatabaseClient = async () => {
     const client = await pool.connect();
     return client;
+}
+
+const dbConfig = {
+    server: String(process.env.PUBLIC_MSSQL_SERVER), // Replace with your SQL Server address
+    database: process.env.PUBLIC_MSSQL_DATABASE, // Replace with your database name
+    user: process.env.PUBLIC_MSSQL_USER, // Replace with your username
+    password: process.env.PUBLIC_MSSQL_PASSWORD, // Replace with your password
+    options: {
+        encrypt: true, // For encrypted connection
+    },
+};
+
+export async function connectToDatabase() {
+    try {
+        const pool = await sql.connect(dbConfig);
+        return pool;
+    } catch (error) {
+        console.error('Database connection error', error);
+        throw error;
+    }
 }
